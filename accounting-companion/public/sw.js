5b7649c1ece5991837bbdb570220b460eaf85aaf
OliverLedesma <draszil.1720@gmail.com>
2026-04-03 09:28:22 +0800

    updated service worker

--- accounting-companion/public/sw.js
+++ accounting-companion/public/sw.js
@@ -1,11 +1,30 @@
-const CACHE_NAME = "acccalc-v1";
-const URLS_TO_CACHE = ["/", "/manifest.webmanifest"];
+const CACHE_NAME = "acccalc-v2";
+const URLS_TO_CACHE = [
+    "/",
+    "/manifest.webmanifest"
+    ];
 
-self.addEventListener("install", (installEvent) => {
+    self.addEventListener("install", (installEvent) => {
+    self.skipWaiting();
+
     installEvent.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
         return cache.addAll(URLS_TO_CACHE);
         })
+    );
+    });
+
+    self.addEventListener("activate", (activateEvent) => {
+    activateEvent.waitUntil(
+        caches.keys().then((cacheNames) => {
+        return Promise.all(
+            cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+                return caches.delete(cacheName);
+            }
+            })
+        );
+        }).then(() => self.clients.claim())
     );
     });
 
